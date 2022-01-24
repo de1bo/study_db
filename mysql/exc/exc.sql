59d0d03af075fd3a728cf0264e1fac80b6ddd5fe
de1bo <ogsal5765@naver.com>
2022-01-24 11:53:37 +0900

    dfg

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,19 @@
+use world; -- 주석
+show tables;
+
+-- 테이블 생성
+create table member1 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);
+
+-- 테이블 삭제
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdate datetime
+);

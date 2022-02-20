e0313449df244ce0134418e9779b6a742ebe8bfb
de1bo <ogsal5765@naver.com>
2022-02-20 21:30:57 +0900

    sdsd

--- mysql/exc/rpcmd.sql
+++ mysql/exc/rpcmd.sql
@@ -11,14 +11,13 @@ from infauthmenu a
 	where 1=1
 		and a.ifatSeq = 1;
         
-        
 select 
 a.ifatSeq,
-(select ifatName from infrAuth where ifatSeq = a.ifatSeq) as ifmuName
+(select ifatName from infrAuth where ifatSeq = a.ifatSeq) as ifmuName12
 , a.ifmuSeq
-, (select ifmuName from infrMenu where ifmuSeq = a.ifmuSeq) as ifmuName
+, (select ifmuName from infrMenu where ifmuSeq = a.ifmuSeq) as ifmuName12
 , a.ifatSeq
-, (select ifcdName from infrCode where ifcdSeq = a.ifauRolCd) as ifauRoleName
+, (select ifcdName from infrCode where ifcdSeq = a.ifauRolCd) as ifauRoleName12
 from infauthmenu a
 	where 1=1
 		and a.ifatSeq = 1;
